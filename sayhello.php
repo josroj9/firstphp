051327e6ea694d2a3e8b962a6d92907dbdd32ecb
josroj9 <josue@lrsperforms.com>
2016-05-29 10:40:03 -0700

    Add files via upload

---
+++ sayhello.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Hello there!\n";
+echo "Why don't you type in your name for me: \n";
+$name = trim(fgets(STDIN));
+
+echo "n\Thanks, ".$name." , it's really nice to meet you \n\n";
+
+?>
